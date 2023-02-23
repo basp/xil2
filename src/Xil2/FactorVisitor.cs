da1c976661f838913cd8d0ee232359d75fa4e009
Bas Pennings <baspster@gmail.com>
2023-02-23 16:42:36 +0100

    Add positions to all factor nodes

--- src/Xil2/FactorVisitor.cs
+++ src/Xil2/FactorVisitor.cs
@@ -1,6 +1,7 @@
 namespace Xil2;
 
 using System.Globalization;
+using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 
 public class FactorVisitor : JoyBaseVisitor<Node>
@@ -11,11 +12,7 @@ public class FactorVisitor : JoyBaseVisitor<Node>
         var value = bool.Parse(context.GetText());
         return new Node.Boolean(value)
         {
-            Position = new Position
-            {
-                Line = context.Start.Line,
-                Column = context.Start.Column,
-            },
+            Position = GetPosition(context),
         };
     }
 
@@ -23,28 +20,40 @@ public class FactorVisitor : JoyBaseVisitor<Node>
         [NotNull] JoyParser.IntegerConstantContext context)
     {
         var value = int.Parse(context.GetText());
-        return new Node.Integer(value);
+        return new Node.Integer(value)
+        {
+            Position = GetPosition(context),
+        };
     }
 
     public override Node VisitFloatConstant(
         [NotNull] JoyParser.FloatConstantContext context)
     {
         var value = double.Parse(context.GetText(), new CultureInfo("en-US"));
-        return new Node.Float(value);
+        return new Node.Float(value)
+        {
+            Position = GetPosition(context),
+        };
     }
 
     public override Node VisitStringConstant(
         [NotNull] JoyParser.StringConstantContext context)
     {
         var value = context.GetText().Trim('"');
-        return new Node.String(value);
+        return new Node.String(value)
+        {
+            Position = GetPosition(context),
+        };
     }
 
     public override Node VisitAtomicSymbol(
         [NotNull] JoyParser.AtomicSymbolContext context)
     {
         var name = context.GetText();
-        return new Node.Symbol(name);
+        return new Node.Symbol(name)
+        {
+            Position = GetPosition(context),
+        };
     }
 
     public override Node VisitQuotationLiteral(
@@ -54,9 +63,20 @@ public class FactorVisitor : JoyBaseVisitor<Node>
         foreach (var factor in context.term().factor())
         {
             var node = factor.Accept(this);
+            node.Position = GetPosition(factor);
             elements.Add(node);
         }
 
-        return new Node.List(elements);
+        return new Node.List(elements)
+        {
+            Position = GetPosition(context),
+        };
     }
+
+    private static Position GetPosition(ParserRuleContext context) =>
+        new Position
+        {
+            Line = context.Start.Line,
+            Column = context.Start.Column,
+        };
 }
